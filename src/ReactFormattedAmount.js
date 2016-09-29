2419393c36c833eb5396ef810582c569e7e18e3b
Julien TASSIN <julie@tassin.work>
2016-09-29 20:53:26 +0200

    add tests

--- src/ReactFormattedAmount.js
+++ src/ReactFormattedAmount.js
@@ -1,9 +1,34 @@
-var React = require('react');
+import React, { PropTypes, PureComponent } from 'react';
 
-var ReactFormattedAmount = React.createClass({
-	render () {
-		return <div>react-formatted-amount</div>;
+class FormattedAmount extends PureComponent {
+
+	static propTypes = {
+		amount: PropTypes.number.isRequired,
+		currency: PropTypes.string.isRequired,
+	};
+
+	render() {
+		let result = '';
+		const styles = {
+			negative: {
+				color: 'red',
+			},
+		};
+		const amount = Math.abs(this.props.amount / 100);
+		const formattedAmount = amount.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ');
+		if (this.props.amount) {
+			if (this.props.amount > 0) {
+				result = <span>{formattedAmount} {this.props.currency}</span>;
+			} else {
+				result = (<span style={styles.negative}>
+				({formattedAmount} {this.props.currency})
+			</span>);
+			}
+		} else {
+			result = <span>0.00 {this.props.currency}</span>;
+		}
+		return result;
 	}
-});
+}
 
-export default ReactFormattedAmount;
+export default FormattedAmount;
