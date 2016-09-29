60664aadfd5e93f467c1591a03ae74624d9155db
Julien TASSIN <julie@tassin.work>
2016-09-30 00:51:50 +0200

    devs

--- example/src/example.js
+++ example/src/example.js
@@ -1,15 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactFormattedAmount = require('react-formatted-amount');
+import Example1Raw from 'raw!./example1';
+import Example1Comp from './example1';
+import ReactFormattedAmount from '../../src/ReactFormattedAmount';
+import githubLight from './github-light.css';
+import normalize from './normalize.css';
+import stylesheet from './stylesheet.css';
+import Playground  from 'component-playground';
 
-var App = React.createClass({
+var Example1 = React.createClass({ 
 	render () {
 		return (
-			<div>
-				<ReactFormattedAmount />
+			<div style={{backgroundColor: 'white'}}>
+				<Playground codeText={"<ReactFormattedAmount amount={1337} currency={'$'}/>"} scope={{React: React, ReactFormattedAmount: ReactFormattedAmount}}/>
 			</div>
 		);
 	}
 });
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Example1 />, document.getElementById('example1'));
+
