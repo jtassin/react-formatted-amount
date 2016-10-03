038d94ceb9da5a8b60204f1923ab6f4dc72a39e9
Julien TASSIN <julie@tassin.work>
2016-10-03 08:32:28 +0200

    devs

--- example/src/example.js
+++ example/src/example.js
@@ -1,7 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-import Example1Raw from 'raw!./example1';
-import Example1Comp from './example1';
 import ReactFormattedAmount from '../../src/ReactFormattedAmount';
 import githubLight from './github-light.css';
 import normalize from './normalize.css';
@@ -20,3 +18,27 @@ var Example1 = React.createClass({
 
 ReactDOM.render(<Example1 />, document.getElementById('example1'));
 
+
+var Example2 = React.createClass({
+	render () {
+		return (
+			<div style={{backgroundColor: 'white'}}>
+				<Playground codeText={"<ReactFormattedAmount amount={-1337} currency={'£'}/>"} scope={{React: React, ReactFormattedAmount: ReactFormattedAmount}}/>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<Example2 />, document.getElementById('example2'));
+
+var Example3 = React.createClass({
+	render () {
+		return (
+			<div style={{backgroundColor: 'white'}}>
+				<Playground codeText={"<ReactFormattedAmount amount={1337003658} currency={'€'}/>"} scope={{React: React, ReactFormattedAmount: ReactFormattedAmount}}/>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<Example3 />, document.getElementById('example3'));
