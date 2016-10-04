d3f9dd9fb5dae9657e0b4004bb866aea41e1bffb
Julien TASSIN <julie@tassin.work>
2016-10-04 20:32:37 +0200

    eslint + release with npm

--- src/ReactFormattedAmount.js
+++ src/ReactFormattedAmount.js
@@ -2,33 +2,31 @@ import React, { PropTypes, PureComponent } from 'react';
 
 class FormattedAmount extends PureComponent {
 
-	static propTypes = {
-		amount: PropTypes.number.isRequired,
-		currency: PropTypes.string.isRequired,
-	};
+  static propTypes = {
+    amount: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+  };
 
-	render() {
-		let result = '';
-		const styles = {
-			negative: {
-				color: 'red',
-			},
-		};
-		const amount = Math.abs(this.props.amount / 100);
-		const formattedAmount = amount.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ');
-		if (this.props.amount) {
-			if (this.props.amount > 0) {
-				result = <span>{formattedAmount} {this.props.currency}</span>;
-			} else {
-				result = (<span style={styles.negative}>
-				({formattedAmount} {this.props.currency})
-			</span>);
-			}
-		} else {
-			result = <span>0.00 {this.props.currency}</span>;
-		}
-		return result;
-	}
+  render() {
+    let result = '';
+    const styles = {
+      negative: {
+        color: 'red',
+      },
+    };
+    const amount = Math.abs(this.props.amount / 100);
+    const formattedAmount = amount.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ');
+    if (this.props.amount) {
+      if (this.props.amount > 0) {
+        result = <span>{formattedAmount} {this.props.currency}</span>;
+      } else {
+        result = (<span style={styles.negative}>({formattedAmount} {this.props.currency})</span>);
+      }
+    } else {
+      result = <span>0.00 {this.props.currency}</span>;
+    }
+    return result;
+  }
 }
 
 export default FormattedAmount;
--- src/__tests__/ReactFormattedAmount.test.js
+++ src/__tests__/ReactFormattedAmount.test.js
@@ -3,65 +3,61 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import ReactFormattedAmount from '../ReactFormattedAmount';
 
+/* eslint-disable no-undef */
 describe('ReactFormattedAmount', () => {
+  it('0 pad amount and add the correct currency symbol', () => {
+    let wrapper = shallow(
+      <ReactFormattedAmount amount={200} currency="€" />
+    );
+    expect(wrapper.html()).to.equal('<span>2.00 €</span>');
 
-	it('0 pad amount and add the correct currency symbol', () => {
-		let wrapper = shallow(
-			<ReactFormattedAmount amount={200} currency='€'/>
-		);
-		expect(wrapper.html()).to.equal('<span>2.00 €</span>');
+    wrapper = shallow(
+      <ReactFormattedAmount amount={210} currency="$" />
+    );
+    expect(wrapper.html()).to.equal('<span>2.10 $</span>');
 
-		wrapper = shallow(
-			<ReactFormattedAmount amount={210} currency='$'/>
-		);
-		expect(wrapper.html()).to.equal('<span>2.10 $</span>');
+    wrapper = shallow(
+      <ReactFormattedAmount amount={211} currency="€" />
+    );
+    expect(wrapper.html()).to.equal('<span>2.11 €</span>');
 
-		wrapper = shallow(
-			<ReactFormattedAmount amount={211} currency='€'/>
-		);
-		expect(wrapper.html()).to.equal('<span>2.11 €</span>');
+    wrapper = shallow(
+      <ReactFormattedAmount amount={211.1} currency="€" />
+    );
+    expect(wrapper.html()).to.equal('<span>2.11 €</span>');
+  });
 
-		wrapper = shallow(
-			<ReactFormattedAmount amount={211.1} currency='€'/>
-		);
-		expect(wrapper.html()).to.equal('<span>2.11 €</span>');
+  it('display the amount in red with parenthesis if negative amount', () => {
+    const wrapper = shallow(
+      <ReactFormattedAmount amount={-200} currency="€" />
+    );
+    expect(wrapper.html()).to.equal('<span style="color:red;">(2.00 €)</span>');
+  });
 
-	});
+  it('display formatted amount greater than 1000 or 1000000', () => {
+    let wrapper = shallow(
+      <ReactFormattedAmount amount={-200000} currency="€" />
+    );
+    expect(wrapper.html()).to.equal('<span style="color:red;">(2 000.00 €)</span>');
+    wrapper = shallow(
+      <ReactFormattedAmount amount={-2000000} currency="€" />
+    );
+    expect(wrapper.html()).to.equal('<span style="color:red;">(20 000.00 €)</span>');
+    wrapper = shallow(
+      <ReactFormattedAmount amount={-20000000} currency="€" />
+    );
+    expect(wrapper.html()).to.equal('<span style="color:red;">(200 000.00 €)</span>');
+    wrapper = shallow(
+      <ReactFormattedAmount amount={-200000000} currency="€" />
+    );
+    expect(wrapper.html()).to.equal('<span style="color:red;">(2 000 000.00 €)</span>');
+  });
 
-	it('display the amount in red with parenthesis if negative amount', () => {
-		let wrapper = shallow(
-			<ReactFormattedAmount amount={-200} currency='€'/>
-		);
-		expect(wrapper.html()).to.equal('<span style="color:red;">(2.00 €)</span>');
-
-
-	});
-	
-	it('display formatted amount greater than 1000 or 1000000', () => {
-		let wrapper = shallow(
-			<ReactFormattedAmount amount={-200000} currency='€'/>
-		);
-		expect(wrapper.html()).to.equal('<span style="color:red;">(2 000.00 €)</span>');
-		wrapper = shallow(
-			<ReactFormattedAmount amount={-2000000} currency='€'/>
-		);
-		expect(wrapper.html()).to.equal('<span style="color:red;">(20 000.00 €)</span>');
-		wrapper = shallow(
-			<ReactFormattedAmount amount={-20000000} currency='€'/>
-		);
-		expect(wrapper.html()).to.equal('<span style="color:red;">(200 000.00 €)</span>');
-		wrapper = shallow(
-			<ReactFormattedAmount amount={-200000000} currency='€'/>
-		);
-		expect(wrapper.html()).to.equal('<span style="color:red;">(2 000 000.00 €)</span>');
-	});
-
-	it('display 0 amount like positive amounts', () => {
-		let wrapper = shallow(
-			<ReactFormattedAmount amount={0} currency='USD'/>
-		);
-		expect(wrapper.html()).to.equal('<span>0.00 USD</span>');
-
-
-	});
+  it('display 0 amount like positive amounts', () => {
+    const wrapper = shallow(
+      <ReactFormattedAmount amount={0} currency="USD" />
+    );
+    expect(wrapper.html()).to.equal('<span>0.00 USD</span>');
+  });
 });
+
