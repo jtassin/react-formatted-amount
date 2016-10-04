adcd42629d055a65d6646191d10fa98333d291eb
Julien TASSIN <julie@tassin.work>
2016-10-04 22:10:26 +0200

    example become examples

--- webpack.example.config.js
+++ webpack.example.config.js
@@ -3,22 +3,26 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+const DEBUG = !process.argv.includes('--release');
+console.log(DEBUG);
+
+const entries = ['./examples/src/example'];
+if (DEBUG) {
+  entries.unshift('react-hot-loader/patch');
+  entries.unshift('webpack-dev-server/client?http://localhost:3000');
+  entries.unshift('webpack/hot/only-dev-server');
+}
 
 module.exports = {
 	devtool: 'eval',
-	entry: [
-		'react-hot-loader/patch',
-		'webpack-dev-server/client?http://localhost:3000',
-		'webpack/hot/only-dev-server',
-		'./example/src/example'
-	],
+	entry: entries,
 	output: {
 		path: path.join(__dirname, 'dist/examples'),
 		filename: '[name].js',
 	},
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebpackPlugin({template: 'example/src/index.html'}),
+		new HtmlWebpackPlugin({template: 'examples/src/index.html'}),
 		new ExtractTextPlugin("[name].css")
 	],
 	module: {
